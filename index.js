ac56704d2881753698ec6b652028f55ee6a6e001
AVINASH REDDY K <48565975+buzzer246@users.noreply.github.com>
2025-05-03 17:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as ReactDOMClient from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+
 import '@fontsource/poppins';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
-import './styles/GlobalStyles.css';
 import { store } from './app/store';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>
+  </React.StrictMode>
+)
